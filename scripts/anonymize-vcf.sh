21c27cb153cc09c68cbecfee41b059153d9cf186
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-02-15 09:45:31 +0100

    Improve anonymizing script
    * Better comments
    * Usage info
    * Make sure script works regardless of path to VCF file
    * Use `case` for pattern matching filename
    * Align lines in `awk` script
    * Fix output file path

--- scripts/anonymize-vcf.sh
+++ scripts/anonymize-vcf.sh
@@ -1,23 +1,33 @@
 #!/bin/bash -x
 
 # Script to remove individuals' data from a VCF file.
-# Output file will be compressed and a tabix index will be created.
+# The output file will be compressed and a tabix index will be created.
+# The resulting file is created in the same directory as the original
+# VCF file, but with the string 'anon-' prepended to its name.
 
-vcf="$1"
+# Usage:
+#
+#   anonymize-vcf.sh path/to/file.vcf
 
-if [[ ${vcf%.gz} == "$vcf" ]]; then
-    prefilter="cat"
-    outfile="anon-$vcf.gz"
-else
-    prefilter="zcat"
-    outfile="anon-$vcf"
-fi
+vcf_path="$1"
 
-$prefilter "$vcf" |
+vcf_name="${vcf_path##*/}"
+vcf_dir="${vcf_path%/*}"
+
+case "${vcf_name}" in
+    *.gz)   prefilter="zcat"
+            outfile="anon-$vcf_name"
+            ;;
+    *)      prefilter="cat"
+            outfile="anon-$vcf_name.gz"
+            ;;
+esac
+
+$prefilter "$vcf_path" |
 awk '
     BEGIN   { OFS = "\t" }
     /^##/   { print; next }
-        { print $1,$2,$3,$4,$5,$6,$7,$8 }' |
-bgzip >"$outfile"
+            { print $1,$2,$3,$4,$5,$6,$7,$8 }' |
+bgzip >"$vcf_dir/$outfile"
 
-tabix -p vcf "$outfile"
+tabix -p vcf "$vcf_dir/$outfile"
