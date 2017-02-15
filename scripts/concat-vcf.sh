50163668d6b5443307b69ec2b2fa494f6e7d4f57
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2017-02-15 09:51:07 +0100

    Add more comments and usage info

--- scripts/concat-vcf.sh
+++ scripts/concat-vcf.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -x
 
 # Script to concatenate two or more VCF files.
-# Will create "swegen.vcf.gz" and a corresponding Tabix index.
+# Will create "swegen.vcf.gz" and a corresponding Tabix index in the
+# current directory.
+
+# Usage:
+#
+#   concat-vcf.sh path/to/file1.vcf path/to/file2.vcf
 
 # We use bcftools from a GitHub checkout since the Ubuntu version is too old.
 bcftools_path="/home/andkaha/bcftools"
